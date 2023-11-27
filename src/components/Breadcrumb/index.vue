ebaccfaa24ce81c2efb2d3ddb56c0ab77e263a3a
Alouger <850022303@qq.com>
2023-11-27 13:14:35 +0800

    feat(Breadcrumb): 依据动态数据，渲染面包屑，并处理面包屑变化时的动画

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,27 +1,38 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <!-- 面包屑的最后一项 -->
-    <el-breadcrumb-item>
-      <span class="no-redirect">活动详情</span>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item
+        v-for="(item, index) in breadcrumbData"
+        :key="item.path"
+      >
+        <!-- 不可点击的面包屑项，一般面包屑的最后一项不可点击 -->
+        <span class="no-redirect" v-if="index === breadcrumbData.length - 1">{{
+          item.meta.title
+        }}</span>
+        <!-- 可点击的面包屑项 -->
+        <span class="redirect" v-else @click="onLinkClick(item)">{{
+          item.meta.title
+        }}</span>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
 <script setup>
 import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
 const route = useRoute()
+const router = useRouter()
+
 // 生成数组数据
 const breadcrumbData = ref([])
 const getBreadcrumbData = () => {
   // route.matched 可以获得当前路由的路由表，当前路由的标准化路由记录
   breadcrumbData.value = route.matched.filter(
     // 带有meta和title属性的就可以在面包屑展示
-    item => item.meta && item.meta.title
+    (item) => item.meta && item.meta.title
   )
   console.log(breadcrumbData.value)
 }
@@ -35,6 +46,16 @@ watch(
     immediate: true
   }
 )
+
+// 处理点击事件
+const onLinkClick = (item) => {
+  console.log(item)
+  router.push(item.path)
+}
+
+// 将来需要进行主题替换，所以这里获取下动态样式，hover的颜色我们设置为主色
+const store = useStore()
+const linkHoverColor = ref(store.getters.cssVar.menuBg)
 </script>
 
 <style lang="scss" scoped>
@@ -43,6 +64,17 @@ watch(
   font-size: 14px;
   line-height: 50px;
   margin-left: 8px;
+
+  .redirect {
+    color: #666;
+    // 加粗
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  .redirect:hover {
+    color: v-bind(linkHoverColor);
+  }
 
   ::v-deep .no-redirect {
     color: #97a8be;

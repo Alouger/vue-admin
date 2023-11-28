b2221f5a2a45691554572686bf6d7368bea43a2d
Alouger <850022303@qq.com>
2023-11-28 17:05:49 +0800

    feat(Theme): 实现element-plus主题变更

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -4,10 +4,19 @@
   </div>
 
   <el-pagination
-      :page-size="100"
-      layout="total, prev, pager, next"
-      :total="1000"
-    />
+    :page-size="100"
+    layout="total, prev, pager, next"
+    :total="1000"
+  />
+
+  <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
 
 <script setup>
@@ -18,7 +27,6 @@
 // onMounted(() => {
 //   console.log('pagination', pagination.value)
 // })
-
 </script>
 
 <style lang="scss" scoped></style>

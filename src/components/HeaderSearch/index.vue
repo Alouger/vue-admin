92439ea1f7a94932a0269c9b8f48f4192b04dffe
Alouger <850022303@qq.com>
2023-11-29 10:06:48 +0800

    feat(HeaderSearch): 完成HeaderSearch功能，包括由路由表数据生成数据源，对检索数据源进行模糊搜索

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -1,116 +1,3 @@
-<!-- <template>
-  <div :class="{ show: isShow }" class="header-search">
-    <svg-icon
-      class="search-icon"
-      icon="search"
-      @click.stop="onShowClick"
-    />
-    <el-select
-      ref="headerSearchSelectRef"
-      class="header-search-select"
-      v-model="search"
-      filterable
-      default-first-option
-      remote
-      placeholder="Search"
-      :remote-method="querySearch"
-      @change="onSelectChange"
-    >
-      <el-option
-        v-for="option in 5"
-        :key="option"
-        :label="option"
-        :value="option"
-      ></el-option>
-    </el-select>
-  </div>
-</template>
-
-<script setup>
-import { ref, watch } from 'vue'
-
-// 控制 search 显示
-const isShow = ref(false)
-// el-select 实例
-const headerSearchSelectRef = ref(null)
-const onShowClick = () => {
-  isShow.value = !isShow.value
-  headerSearchSelectRef.value.focus()
-}
-
-/**
- * 关闭 search 的处理事件
- */
-const onClose = () => {
-  // debugger
-  headerSearchSelectRef.value.blur()
-  isShow.value = false
-  // searchOptions.value = []
-}
-/**
- * 监听 search 打开，处理 close 事件
- */
-watch(isShow, (val) => {
-  // debugger
-  console.log(val)
-  if (val) {
-    document.body.addEventListener('click', onClose)
-  } else {
-    document.body.removeEventListener('click', onClose)
-  }
-})
-
-// search 相关
-const search = ref('')
-// 搜索方法
-const querySearch = () => {
-  console.log('querySearch')
-}
-// 选中回调
-const onSelectChange = () => {
-  console.log('onSelectChange')
-}
-</script>
-
-<style lang="scss" scoped>
-.header-search {
-  // font-size: 0 !important;
-  .search-icon {
-    cursor: pointer;
-    font-size: 18px;
-    vertical-align: middle;
-    display: inline-block;
-  }
-  .header-search-select {
-    font-size: 18px;
-    transition: width 0.2s;
-    width: 0;
-    overflow: hidden;
-    background: transparent;
-    border-radius: 0;
-    display: inline-block;
-    vertical-align: middle;
-
-    :deep .el-input__inner {
-      border-radius: 0;
-      border: 0;
-      padding-left: 0;
-      padding-right: 0;
-      box-shadow: none !important;
-      border-bottom: 1px solid #d9d9d9;
-      vertical-align: middle;
-    }
-  }
-  &.show {
-    .header-search-select {
-      width: 210px;
-      margin-left: 10px;
-    }
-  }
-}
-
-</style> -->
-
 <template>
   <div :class="{ show: isShow }" class="header-search">
     <svg-icon
@@ -131,18 +18,60 @@ const onSelectChange = () => {
       @change="onSelectChange"
       show-overflow-tooltip
     >
-    <el-option
-        v-for="option in 5"
-        :key="option"
-        :label="option"
-        :value="option"
+      <el-option
+        v-for="option in searchOptions"
+        :key="option.item.path"
+        :label="option.item.title.join(' > ')"
+        :value="option.item"
       ></el-option>
     </el-select>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
+import { generateRoutes } from './FuseData'
+import { filterRoutes } from '@/utils/route'
+import { useRouter } from 'vue-router'
+import Fuse from 'fuse.js'
+import { watchSwitchLang } from '@/utils/i18n'
+
+// 检索数据源
+const router = useRouter()
+let searchPool = computed(() => {
+  const fRoutes = filterRoutes(router.getRoutes())
+  console.log(generateRoutes(fRoutes))
+  return generateRoutes(fRoutes)
+})
+console.log(searchPool.value)
+
+/**
+ * 搜索库相关
+ */
+let fuse
+const initFuse = (searchPool) => {
+  fuse = new Fuse(searchPool.value, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+initFuse(searchPool)
+console.log(fuse)
 
 // 控制 search 显示
 const isShow = ref(false)
@@ -160,7 +89,7 @@ const onClose = () => {
   // debugger
   headerSearchSelectRef.value.blur()
   isShow.value = false
-  // searchOptions.value = []
+  searchOptions.value = []
 }
 /**
  * 监听 search 打开，处理 close 事件
@@ -177,25 +106,30 @@ watch(isShow, (val) => {
 
 // search 相关
 const search = ref('')
-// 搜索方法
-// const querySearch = () => {
-//   console.log('querySearch')
-// }
 // 搜索结果
-// const searchOptions = ref([])
+const searchOptions = ref([])
 // 搜索方法
 const querySearch = (query) => {
-  // if (query !== '') {
-  //   searchOptions.value = fuse.search(query)
-  // } else {
-  //   searchOptions.value = []
-  // }
+  console.log(fuse.search(query))
+  if (query !== '') {
+    searchOptions.value = fuse.search(query)
+  } else {
+    searchOptions.value = []
+  }
 }
 // 选中回调
 const onSelectChange = (val) => {
-  // router.push(val.path)
+  router.push(val.path)
 }
 
+// 处理国际化
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    const fRoutes = filterRoutes(router.getRoutes())
+    return generateRoutes(fRoutes)
+  })
+  initFuse(searchPool)
+})
 </script>
 
 <style lang="scss" scoped>

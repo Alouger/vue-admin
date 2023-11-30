924fddb5f4f48b9df4d180701de2d91a0c98d702
Alouger <850022303@qq.com>
2023-11-30 17:21:05 +0800

    feat(Profile): 完善个人中心的章节和作者模块

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,19 @@
 import request from '@/utils/request'
 
+/**
+ * 获取项目功能
+ */
 export const features = () => {
   return request({
     url: '/user/feature'
+  })
+}
+
+/**
+ * 获取章节功能
+ */
+export const chapter = () => {
+  return request({
+    url: '/user/chapter'
   })
 }

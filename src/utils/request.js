604ed9aa69e6ea58882fbee77d56155a17c1bcab
Alouger <850022303@qq.com>
2023-11-30 15:38:10 +0800

    feat(Profile): 创建PanThumb头像组件，完成介绍模块

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import { isCheckTimeout } from './auth'
 const service = axios.create({
   // 实现在开发和生产环境下，可以设定不同baseUrl的能力
   baseURL: process.env.VUE_APP_BASE_API,
-  timeout: 5000 * 30
+  timeout: 5000 * 100
 })
 
 // 请求拦截器
--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,11 +1,130 @@
 <template>
-  <div>项目介绍</div>
+  <el-card class="user-container">
+    <template #header>
+      <div>
+        <div class="header">
+          <span>{{ $t('msg.profile.introduce') }}</span>
+        </div>
+      </div>
+    </template>
+
+    <div class="user-profile">
+      <!-- 头像 -->
+      <div class="box-center">
+        <pan-thumb
+          :image="`https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png`"
+          :height="'100px'"
+          :width="'100px'"
+          :hoverable="false"
+        >
+          <div>Hello</div>
+          {{ $store.getters.userInfo.title }}
+        </pan-thumb>
+      </div>
+      <!-- 姓名 && 角色 -->
+      <div class="box-center">
+        <div class="user-name text-center">
+          {{ $store.getters.userInfo.username }}
+        </div>
+        <div class="user-role text-center text-muted">
+          {{ $store.getters.userInfo.title }}
+        </div>
+      </div>
+    </div>
+
+    <!-- 简介 -->
+    <div class="project-bio">
+      <div class="project-bio-section">
+        <div class="project-bio-section-header">
+          <svg-icon icon="introduce" class="introduce-icon" />
+          <span>{{ $t('msg.profile.projectIntroduction') }}</span>
+        </div>
+        <div class="project-bio-section-body">
+          <div class="text-muted">
+            {{ $t('msg.profile.muted') }}
+          </div>
+        </div>
+      </div>
+      <!-- 功能 -->
+      <div class="project-bio-section">
+        <div class="project-bio-section-header">
+          <svg-icon icon="reward" class="reward-icon"/>
+          <span>{{ $t('msg.profile.projectFunction') }}</span>
+        </div>
+        <div class="project-bio-section-body">
+          <div class="progress-item" v-for="item in features" :key="item.id">
+            <div>{{ item.title }}</div>
+            <el-progress :percentage="item.percentage" status="success" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script setup>
+import PanThumb from '@/components/PanThumb'
+import { defineProps } from 'vue'
 
+const props = defineProps({
+  features: {
+    type: Array,
+    required: true
+  }
+})
+console.log(props.features)
 </script>
 
 <style lang="scss" scoped>
+.user-container {
+  .text-muted {
+    font-size: 14px;
+    color: #777;
+  }
 
+  .user-profile {
+    text-align: center;
+    .user-name {
+      font-weight: bold;
+    }
+    .box-center {
+      padding-top: 10px;
+    }
+    .user-role {
+      padding-top: 10px;
+      font-weight: 400;
+    }
+  }
+  .project-bio {
+    margin-top: 20px;
+    color: #606266;
+    span {
+      padding-left: 4px;
+    }
+    .project-bio-section {
+      margin-bottom: 36px;
+      .project-bio-section-header {
+        border-bottom: 1px solid #dfe6ec;
+        padding-bottom: 10px;
+        margin-bottom: 10px;
+        font-weight: bold;
+        .introduce-icon {
+          display: inline-block;
+        }
+        .reward-icon {
+          display: inline-block
+        }
+      }
+      .project-bio-section-body {
+        .progress-item {
+          margin-top: 10px;
+          div {
+            font-size: 14px;
+            margin-bottom: 2px;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
